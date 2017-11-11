2db25429293f048944ce158b2b9a29e6a3e59dbb
glennclarence <glenn.glashagen@gmail.com>
2017-11-11 18:25:24 +0100

    implementet test to check correct reading of modes

---
+++ test/test_Transformation.cpp
@@ -0,0 +1,34 @@
+
+#include <gmock/gmock.h>
+#include "test_HMMread.h"
+#include "transform.h"
+
+namespace as {
+
+TEST(a,s) {
+
+
+	rotate_translate<float>(
+			REAL const* x,
+			REAL const* y,
+			REAL const* z,
+			Vec3<REAL> const& displacement,
+			Vec3<REAL> const& ang,
+			unsigned const& ligSize,
+			unsigned const& numModes,
+			REAL const* dlig,
+			REAL const* xModes,
+			REAL const* yModes,
+			REAL const* zModes,
+			REAL* xTr,
+			REAL* yTr,
+			REAL* zTr)
+	{
+}
+
+
+
+
+
+
+}//end as namespace
