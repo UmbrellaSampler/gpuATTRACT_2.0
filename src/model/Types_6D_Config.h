910c768a2a72dd3af67bfbbfc1754adc34c58386
glennclarence <glenn.glashagen@gmail.com>
2017-11-22 20:41:32 +0100

    small changes

---
+++ src/model/Types_6D_Config.h
@@ -0,0 +1,14 @@
+/*
+ * Types_6D_Config.h
+ *
+ *  Created on: Nov 22, 2017
+ *      Author: glenn
+ */
+
+#ifndef SRC_MODEL_TYPES_6D_CONFIG_H_
+#define SRC_MODEL_TYPES_6D_CONFIG_H_
+
+const unsigned MODES_LIGAND_MAX_NUMBER=10;
+const unsigned MODES_RECEPTOR_MAX_NUMBER=10;
+
+#endif /* TYPES_6D_CONFIG_H_ */
