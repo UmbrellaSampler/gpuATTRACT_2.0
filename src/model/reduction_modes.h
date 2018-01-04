bbeff480e20c29a46bdbe5cfc23b7fa00de5a722
glennclarence <glenn.glashagen@gmail.com>
2018-01-04 17:09:26 +0100

    updated final reduction for the gpu such that it contains the force correction for modes

--- src/model/reduction_modes.h
+++ src/model/reduction_modes.h
@@ -118,16 +118,17 @@ void correctModeForce(
 #ifdef CUDA
 
 template <class T>
-void d_reduce(
+void d_reduceMode(
 		const unsigned& threads,
 		const unsigned& blocks,
 		const unsigned& numAtomsRec,
 		const unsigned& numAtomsLig,
 		const unsigned& numModesRec,
 		const unsigned& numModesLig,
-		T *xModesRec,T *yModesRec,T *zModesRec,
-		T *xModesLig,T *yModesLig,T *zModesLig,
+		 DOF_6D_Modes<T>* dofs,
 		T* xPos, T* yPos, T* zPos,
+		T *xModesLig,T *yModesLig,T *zModesLig,
+		T *xModesRec,T *yModesRec,T *zModesRec,
 		T *d_fxRec, T *d_fyRec, T *d_fzRec,
 		T *d_fxLig, T *d_fyLig, T *d_fzLig,
 		T *d_E,
@@ -143,6 +144,7 @@ void deviceReduce(
 		const unsigned& ligandSize,
 		const unsigned& numModesRec,
 		const unsigned& numModesLig,
+		DOF_6D_Modes<REAL>* dofs,
 		REAL *xModesRec,REAL *yModesRec,REAL *zModesRec,
 		REAL *xModesLig,REAL *yModesLig,REAL *zModesLig,
 		REAL* xPos, REAL* yPos, REAL* zPos,
@@ -153,13 +155,16 @@ void deviceReduce(
 		const cudaStream_t& stream)
 {
 	/* we need at least twice the block size number of threads */
-	const unsigned threads = (max(ligandSize, receptorSize) < blockSize*2) ? nextPow2((max(ligandSize, receptorSize) + 1)/ 2) : blockSize;
+	//const unsigned threads = (max(ligandSize, receptorSize) < blockSize*2) ? nextPow2((max(ligandSize, receptorSize) + 1)/ 2) : blockSize;
+	unsigned size = max(ligandSize, receptorSize);
+	const unsigned threads = (size < blockSize*2) ? nextPow2((size + 1)/ 2) : blockSize;
 	/* each structure is reduced by one thread block */
 	const unsigned blocks = numDOFs;
-	d_reduce(threads, blocks, receptorSize, ligandSize, numModesRec, numModesLig,
+	d_reduceMode(threads, blocks, receptorSize, ligandSize, numModesRec, numModesLig,
+			dofs,
+			xPos, yPos, zPos,
 			xModesRec, yModesRec, zModesRec,
 			xModesLig, yModesLig, zModesLig,
-			xPos, yPos, zPos,
 			d_fxRec, d_fyRec, d_fzRec,
 			d_fxLig, d_fyLig, d_fzLig,
 			d_E,
@@ -169,35 +174,37 @@ void deviceReduce(
 
 /* remaining reduce part that is performed on the host */
 template<typename REAL>
-void h_finalReduce(const unsigned& dofSize,
+void h_finalReduce(
 			const unsigned& numDOFs,
 			DOF_6D_Modes<REAL>* dofs,
+			REAL const* modeForceConstantRec,
+			REAL const* modeForceConstantLig,
 			const unsigned int& numModesRec,
 			const unsigned int& numModesLig,
 			const REAL* deviceOut,
 			Result_6D_Modes<REAL>* enGrads)
 {
-
+	unsigned const dofSize = 13 + numModesRec + numModesLig;
 	for (unsigned i = 0; i < numDOFs; ++i)
 	{
 		auto &enGrad = enGrads[i];
-		enGrad.pos.x = deviceOut[i*dofSize + 0];
-		enGrad.pos.y = deviceOut[i*dofSize + 1];
-		enGrad.pos.z = deviceOut[i*dofSize + 2];
+		enGrad._6D.pos.x = deviceOut[i*dofSize + 0];
+		enGrad._6D.pos.y = deviceOut[i*dofSize + 1];
+		enGrad._6D.pos.z = deviceOut[i*dofSize + 2];
 
 		for(unsigned j = 0; j < 3; ++j) {
-			REAL magn2 = enGrad.pos.x*enGrad.pos.x
-					+ enGrad.pos.y*enGrad.pos.y
-					+ enGrad.pos.z*enGrad.pos.z;
+			REAL magn2 = enGrad._6D.pos.x*enGrad._6D.pos.x
+					+ enGrad._6D.pos.y*enGrad._6D.pos.y
+					+ enGrad._6D.pos.z*enGrad._6D.pos.z;
 
 			if(magn2 > static_cast<REAL>(ForceLim)) {
-				enGrad.pos.x *= 0.01;
-				enGrad.pos.y *= 0.01;
-				enGrad.pos.z *= 0.01;
+				enGrad._6D.pos.x *= 0.01;
+				enGrad._6D.pos.y *= 0.01;
+				enGrad._6D.pos.z *= 0.01;
 			}
 		}
 
-		enGrad.E = deviceOut[i*dofSize + 3];
+		enGrad._6D.E = deviceOut[i*dofSize + 3];
 
 		Torque<REAL> torque;
 		torque.mat[0][0] = deviceOut[i*dofSize + 4 ];
@@ -211,18 +218,32 @@ void h_finalReduce(const unsigned& dofSize,
 		torque.mat[2][2] = deviceOut[i*dofSize + 12];
 
 		for(int mode=0; mode < numModesLig; mode++){
-			enGrad.modesLig=deviceOut[i*dofSize + 13 + mode];
+			enGrad.modesLig[0]=deviceOut[i*dofSize + 13 +mode];
 		}
+
 		for(int mode=0; mode < numModesRec; mode++){
-			enGrad.modesRec=deviceOut[i*dofSize + 13 + numModesRec + mode];
+			enGrad.modesRec[0]=deviceOut[i*dofSize + 13 + numModesLig + mode];
 		}
+		correctModeForce(
+			modeForceConstantRec,
+			numModesRec,
+			enGrad.modesRec
+			);
+
+		correctModeForce(
+			modeForceConstantLig,
+			numModesLig,
+			enGrad.modesLig
+			);
+
+
 		const auto &dof = dofs[i];
-		const TorqueMat<REAL> torqueMat = euler2torquemat(dof.ang.x, dof.ang.y, dof.ang.z);
+		const TorqueMat<REAL> torqueMat = euler2torquemat(dof._6D.ang.x, dof._6D.ang.y, dof._6D.ang.z);
 		Vec3<REAL> result = torqueMat.rotateReduce(torque);
 
-		enGrad.ang.x = result.x;
-		enGrad.ang.y = result.y;
-		enGrad.ang.z = result.z;
+		enGrad._6D.ang.x = result.x;
+		enGrad._6D.ang.y = result.y;
+		enGrad._6D.ang.z = result.z;
 	}
 }
 #endif // CUDA
