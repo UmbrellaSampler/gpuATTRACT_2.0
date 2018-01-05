9d25559ec848f363205449450363f5d55d1cd394
glennclarence <glenn.glashagen@gmail.com>
2018-01-05 13:29:57 +0100

    only namesubstitution

---
+++ src/service/Configurator_MB_6D_Modes.cpp
@@ -0,0 +1,20 @@
+/*
+ * Configurator_MB_6D_Modes.cpp
+ *
+ *  Created on: Dec 7, 2017
+ *      Author: uwe
+ */
+
+#include "Configurator_MB_6D_Modes.tpp"
+
+namespace as {
+
+template
+class Configurator_MB_6D_Modes<float>;
+
+template
+class Configurator_MB_6D_Modes<double>;
+
+}  // namespace as
+
+
